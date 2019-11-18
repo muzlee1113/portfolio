64b9cf39a394389f0996ed078929b94cd2ef14fa
leeven <yw.li@siid.ac.cn>
2019-11-18 09:58:03 -0800

    tagline

--- src/components/LeadingForWork/index.js
+++ src/components/LeadingForWork/index.js
@@ -6,7 +6,7 @@ function LeadingForWork(props) {
         <section className="leading">
           
             <h1>Hi, I<span style={{"color": "#9D2721"}}>’</span>m Yuwen Li<span style={{"color": "#9D2721"}}>.</span></h1>
-            <h3>I'm a product & user experience designer who love categories and codes.</h3>
+            <h3>I’m a user experience designer who brings into shape the right tools that make people’s jobs and lives easier.</h3>
             {/* <ul className="leading-text tabs">
                 <li className="tabs-contain" >UX </li>
                 <li className="tabs-contain" ><a className={"tabs-controls " + props.designer} onClick={props.changePanel} name="designer">Designer </a></li>
