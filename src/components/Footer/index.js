53554a1810642a246b75a480e07efdcd0ce54f63
leeven <yw.li@siid.ac.cn>
2020-01-12 00:04:42 -0800

    resume

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 
 
-function Footer() {
+export default function Footer() {
     return (<>
         <footer>
             <div className="footer_content">
